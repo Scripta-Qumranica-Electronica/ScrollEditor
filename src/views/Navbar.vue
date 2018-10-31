e0f631996a42d9b5ec26b488e139288f97eb9992
Itay Zandbank <itay@chelem.co.il>
2018-10-31 14:20:00 +0200

    Remove elements-ui Add basic navbar component and router-view

---
+++ src/views/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar">
+    <nav>
+    <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+    </ul>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'navbar',
+});
+</script>
