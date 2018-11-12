ede907818c09b733ae00694dceb365edb9870910
Itay Zandbank <itay@chelem.co.il>
2018-11-12 11:56:21 +0200

    Move home view to its own folder

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
+import Home from './views/home/Home.vue';
 
 Vue.use(Router);
 
