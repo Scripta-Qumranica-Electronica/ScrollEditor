b76c0029e524d4c9163455bc6bd59e3809cb0e11
Itay Zandbank <itay@chelem.co.il>
2018-11-12 13:49:49 +0200

    Show scroll type in search only when user is logged in

--- src/views/home/components/SearchScroll.vue
+++ src/views/home/components/SearchScroll.vue
@@ -5,9 +5,11 @@
         <b-button type="submit" size="sm" @click="search" variant="primary" class="ml-2" :disabled="isSearchDisabled">
           <font-awesome-icon icon="search"></font-awesome-icon>
         </b-button>
-        <b-form-checkbox class="ml-3" v-model="searchPublic">Public</b-form-checkbox>
-        <b-form-checkbox v-model="searchShared">Shared</b-form-checkbox>
-        <b-form-checkbox v-model="searchPrivate">Private</b-form-checkbox>
+        <div v-if="loggedIn">
+          <b-form-checkbox class="ml-3" v-model="searchPublic">Public</b-form-checkbox>
+          <b-form-checkbox v-model="searchShared">Shared</b-form-checkbox>
+          <b-form-checkbox v-model="searchPrivate">Private</b-form-checkbox>
+        </div>
     </b-form>
 </template>
 
@@ -40,15 +42,20 @@ export default Vue.extend({
   computed: {
     isSearchDisabled(): boolean {
       return (!this.searchPublic && !this.searchShared && !this.searchPrivate) || !this.searchTerm;
+    },
+    loggedIn(): boolean {
+      return this.$store.state.session.loggedIn;
     }
   },
   methods: {
     search() {
+      const loggedIn = this.$store.state.session.loggedIn();
+
       const searchInfo: SearchInfo = {
         term: this.searchTerm,
-        public: this.searchPublic,
-        shared: this.searchShared,
-        private: this.searchPrivate,
+        public: loggedIn ? this.searchPublic : true,
+        shared: loggedIn ? this.searchShared : false,
+        private: loggedIn ? this.searchPrivate : false,
       };
       this.$emit('searchScroll', searchInfo);
     },
