49ec8476faa7e18672ef2afa782e1b6546f52055
bronsonbdevost <bronsobdevost@aim.com>
2019-01-09 09:32:39 +0000

    Note on naming, reverse of fragment now visible.

--- src/views/scrollver/components/fragment-card.vue
+++ src/views/scrollver/components/fragment-card.vue
@@ -2,9 +2,10 @@
     <div class="card">
         <router-link :to="{ path: `/fragment/${scrollVersionId}/${fragment.uniqueId}` }">
             <img class="card-img-top" v-lazy="imageUrl" v-if="imageUrl" alt="Fragment Image">
+            <--! This shows the back too -->
+            <img class="card-img-top" v-lazy="imageRevUrl" v-if="imageRevUrl" alt="Fragment Image">
         </router-link>
         <label>{{artefactsNames}}</label>
-        <img class="card-img-top" v-lazy="imageUrl" v-if="imageUrl" alt="Fragment Image">
     </div>
 </template>
 
@@ -13,7 +14,12 @@ import Vue from 'vue';
 import { Fragment } from '@/models/fragment';
 
 export default Vue.extend({
-    name: 'scroll-ver-fragments',
+    // This component had the same name as its parent.
+    // I would highly recommend ommitting the name attribute,
+    // then the component will take its name from its filename.
+    // I would also use the filename format FragmentCard.
+    // If you do this, then debugging with Vue Devtools becomes far easier.
+    name: 'scroll-ver-fragment-card',  
     props: {
         fragment: Fragment,
     },
@@ -21,6 +27,13 @@ export default Vue.extend({
         imageUrl(): string | undefined {
             if (this.fragment && this.fragment.recto && this.fragment.recto.master) {
                 return this.fragment.recto.master.getThumbnailUrl(600);
+            }
+            return undefined;
+        },
+        // Now we can see the back too.
+        imageRevUrl(): string | undefined {
+            if (this.fragment && this.fragment.verso && this.fragment.verso.master) {
+                return this.fragment.verso.master.getThumbnailUrl(600);
             }
             return undefined;
         },
