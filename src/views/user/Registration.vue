5ec9a83674b24c0173557b1f3b6a9f1ba2da3206
Talya Shraga <talyashra@gmail.com>
2019-05-07 16:49:01 +0300

    user management

---
+++ src/views/user/Registration.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    Registartion
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'registration',
+  components: {
+  },
+  data() {
+
+  },
+  computed: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+});
+</script>
+
+<style scoped>
+.active-language {
+  font-weight: bold;
+}
+</style>
