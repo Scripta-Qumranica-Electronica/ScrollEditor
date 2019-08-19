bd850970c2a801331cf70bdd584d1ad8b8fb25c6
talyashra <talyashra@gmail.com>
2019-08-19 18:31:00 +0300

    Image settings in artefact editor

---
+++ src/components/image-settings/ImageSettings.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <!-- <pre>{{imageStack.availableImageTypes}}</pre> -->
+      <!-- <pre>{{params}}</pre> -->
+      <single-image-setting v-for="imageType in imageStack.availableImageTypes" :key="imageType" 
+                :type="imageType" :settings="params.imageSettings[imageType]" @change="onImageSettingChanged($event)">
+    </single-image-setting>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import SingleImageSettingComponent from '@/components/editors/SingleImageSetting.vue';
+import { ImagedObject } from '../../models/imaged-object';
+import { ImageStack } from '../../models/image';
+import { EditorParams } from '../../views/imaged-object-editor/types';
+import { SingleImageSetting } from './types';
+import { ArtefactEditorParams } from '../../views/artefact-editor/types';
+
+export default Vue.extend({
+  name: 'image-settings',
+  components: {
+    'single-image-setting': SingleImageSettingComponent,
+  },
+  props: {
+    imageStack: ImageStack,
+    params: ArtefactEditorParams // TODO_ change type of params EditorParams,
+  },
+  data() {
+    return {
+      errorMessage: '',
+    };
+  },
+  methods: {
+      onImageSettingChanged($event: any) {
+          const singleImgSetting = $event as SingleImageSetting;
+          this.$emit('imageSettingChanged', singleImgSetting);
+      }
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
