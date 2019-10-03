b5ef208a640ddd17a47aa30a84a7ce5c7dff6239
Itay Zandbank <itay@chelem.co.il>
2019-10-03 11:05:01 +0300

    Remove unnecessary service methods.

--- src/services/artefact.ts
+++ src/services/artefact.ts
@@ -1,34 +1,12 @@
 import { CommHelper } from './comm-helper';
-import { StateManager } from '@/state';
 import { ImagedObject } from '@/models/imaged-object';
 import { ArtefactListDTO, CreateArtefactDTO, ArtefactDTO, UpdateArtefactDTO } from '@/dtos/sqe-dtos';
 import { Artefact } from '@/models/artefact';
-import EditionService from './edition';
 import { ApiRoutes } from '@/services/api-routes';
 import { Side } from '@/models/misc';
 import { OptimizedArtefact } from '@/views/imaged-object-editor/types';
 
 class ArtefactService {
-    public stateManager: StateManager;
-    constructor() {
-        this.stateManager = StateManager.instance;
-    }
-
-    public async getArtefact(editionId: number, artefactId: number) {
-        let artefact = this._getCachedArtefact(editionId, artefactId);
-        if (!artefact) {
-            artefact = await this._getArtefact(editionId, artefactId);
-        }
-
-        if (!artefact) {
-            throw new Error(`Can't find artefact with id ${artefactId}`);
-        }
-
-        this.stateManager.artefacts.current = artefact;
-
-        return artefact;
-    }
-
     public async getEditionArtefacts(editionId: number): Promise<Artefact[]> {
         const response = await CommHelper.get<ArtefactListDTO>(
             ApiRoutes.allEditionArtefactsUrl(editionId, true)
@@ -75,24 +53,6 @@ class ArtefactService {
         const response = await CommHelper.put<ArtefactDTO>
             (ApiRoutes.editionArtefactUrl(editionId, artefact.id), body);
         return response.data;
-    }
-
-    private _getCachedArtefact(editionId: number, artefactId: number): Artefact | undefined {
-        if (!this.stateManager.editions.current || editionId !== this.stateManager.editions.current.id) {
-            return undefined;
-        }
-        if (!this.stateManager.artefacts.items) {
-            return undefined;
-        }
-
-        return this.stateManager.artefacts.items.find((a: Artefact) => a.id === artefactId);
-    }
-
-    private async _getArtefact(editionId: number, artefactId: number) {
-        const editionService = new EditionService();
-        const artefacts = await this.getEditionArtefacts(editionId);
-
-        return artefacts.find((a: Artefact) => a.id === artefactId);
     }
 }
 
--- src/services/imaged-object.ts
+++ src/services/imaged-object.ts
@@ -1,62 +1,16 @@
 import { ImagedObject } from '@/models/imaged-object';
-import EditionService from './edition';
 import { CommHelper } from './comm-helper';
 import { ApiRoutes } from '@/services/api-routes';
-import { StateManager } from '@/state';
-import { Artefact } from '@/models/artefact';
-import { ImagedObjectDTO, ImagedObjectListDTO } from '@/dtos/sqe-dtos';
+import { ImagedObjectListDTO } from '@/dtos/sqe-dtos';
 
 
 class ImagedObjectService {
-    public stateManager: StateManager;
-    constructor() {
-        this.stateManager = StateManager.instance;
-    }
-
-    public async getImagedObjectArtefacts(editionId: number, imagedObjectId: string): Promise<Artefact[]> {
-        const response = await CommHelper.get<ImagedObjectDTO>(
-            ApiRoutes.editionImagedObjectUrl(editionId, imagedObjectId, true)
-        );
-
-        let artefactList: Artefact[] = [];
-        if (response.data.artefacts) {
-            artefactList = response.data.artefacts.map((obj: any) => new Artefact(obj));
-        }
-
-        return artefactList;
-    }
-
     public async getEditionImagedObjects(editionId: number): Promise<ImagedObject[]> {
         const response = await CommHelper.get<ImagedObjectListDTO>(
             ApiRoutes.allEditionImagedObjectsUrl(editionId, true)
         );
 
         return response.data.imagedObjects.map((d: any) => new ImagedObject(d));
-    }
-
-    // The position is a transform matrix for positioning the
-    // artifact properly in the coordinate system of the virtual scroll.
-    // todo: changeArtefact position: add position to the body
-    // const transformMatrix = artefact.transformMatrix ?
-    //     artefact.transformMatrix :
-    //     '{"matrix": [[1, 0, 0], [0, 1, 0]]}';
-
-    private _getCachedImagedObject(editionId: number, imagedObjectId: string): ImagedObject | undefined {
-        if (!this.stateManager.editions.current || editionId !== this.stateManager.editions.current.id) {
-            return undefined;
-        }
-        if (!this.stateManager.imagedObjects.items) {
-            return undefined;
-        }
-
-        return this.stateManager.imagedObjects.items.find((io: ImagedObject) => io.id === imagedObjectId);
-    }
-
-    private async _getImagedObject(editionId: number, imagedObjectId: string) {
-        const editionService = new EditionService();
-        const imagedObjectList = await this.getEditionImagedObjects(editionId);
-
-        return imagedObjectList.find((io: ImagedObject) => io.id === imagedObjectId);
     }
 }
 
