7f590025764983ca7745517ff312f357476e2045
shaindel <shaindel.barkats@hotmail.com>
2019-11-29 00:10:01 +0200

    event  Rotate  part 1

--- src/components/misc/zoomer.vue
+++ src/components/misc/zoomer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div @wheel="onWheel($event)" v-hammer:pinch="onPinch">
+    <div @wheel="onWheel($event)" v-hammer:pinch="onPinch" v-hammer:rotate="onRotate" v-hammer:rotatestart="onRotateStart" v-hammer:rotateend="onRotateEnd">
         <slot></slot>
     </div>
 </template>
@@ -17,27 +17,41 @@
  */
 import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
 import { Point } from '@/utils/helpers';
+// import { VueHammer } from 'vue2-hammer'
 
 export interface ZoomEventArgs {
     zoom: number;
+}
+
+export interface RotateEventArgs {
+    rotate: number;
+    
 }
 
 @Component({
     name: 'zoomer',
 })
 export default class Zoomer extends Vue {
+
+     private degel=false;
     @Prop() private zoom!: number;
+    @Prop() private angle!: number;
 
     @Emit()
     private newZoom(zoom: number): ZoomEventArgs {
         return { zoom };
     }
 
+    @Emit()
+    private newRotate(rotate: number): RotateEventArgs {
+        return { rotate };
+    }
     private onWheel(event: WheelEvent) {
         if (!event.ctrlKey) {
             return;
         }
-
+     
+       
         event.preventDefault(); // Don't use the browser's zoom mechanism here, just ours
         const amount = event.deltaY < 0 ? +0.01 : -0.01; // wheel up - zoom in.
 
@@ -56,6 +70,11 @@ export default class Zoomer extends Vue {
     private applyZoom(amount: number, position: Point) {
         const oldZoom = this.zoom;
         const newZoom = Math.min(Math.max(oldZoom + amount, 0.05), 1);
+
+        if(this.degel==true){
+            return;
+        }
+
         if (newZoom === oldZoom) {
             return;
         }
@@ -90,6 +109,34 @@ export default class Zoomer extends Vue {
 
     private get zoomTarget(): Element {
         return this.$el.parentElement!;
+    }
+
+    private onRotateStart(event:any){
+        console.log('rotate start');
+        this.degel = true;
+    }
+
+    private onRotateEnd(event:any){
+        console.log('rotate end');
+        this.degel = false;
+    }
+
+    private onRotate(event: any) {
+      
+        console.log(event);
+  
+      
+       const angleCalc =(event.angle)/6;
+       console.log(angleCalc,"aaaaangleCalc");
+       this.newRotate(angleCalc);
+
+
+
+
+        // console.log(event);
+        // const angleCalc = event.angle + this.angle;
+
+        // this.newRotate(angleCalc);
     }
 }
 </script>
