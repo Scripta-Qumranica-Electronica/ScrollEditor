60b60c0460f586faca2bd96370963452721d9bac
shaindel <shaindel.barkats@hotmail.com>
2019-12-10 13:47:58 +0200

    continue test login

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -5,43 +5,87 @@ describe('The Home Page', function() {
 
 
     })
-    it('Login to application', () => {
-        const password = 'test'
-        const email = 'test@1.com'
-        cy.contains('button', 'Login').click()
 
-        cy.get('input[type=email]').type(email)
-            .should('have.value', email)
+    Cypress.Commands.add('typeLogin', (user) => {
+        cy.get('input[type=email]')
+            .type(user.email)
+        cy.get('input[type=password]')
+            .type(user.password)
+    })
 
-
-        cy.get('input[type=password]').type(password)
-            .should('have.value', password)
-
-
+    Cypress.Commands.add('PostLogin', () => {
         cy.server()
         cy.route('POST', '/v1/users/login').as('postUser')
         cy.get('button[type=submit]').click()
         cy.wait('@postUser')
+    })
 
-        cy.get('@postUser').should((resp) => {
-            expect(resp.status).to.eq(200)
+    it('Login Fails', () => {
+
+            cy.contains('button', 'Login').click()
+
+            cy.typeLogin({ email: 'tests@1.com', password: 'tests' })
+
+            cy.PostLogin()
+
+            cy.get('@postUser').should((resp) => {
+                expect(resp.status).to.eq(401)
+            })
+
         })
+        // it('Login Fails Data ', () => {
 
-        // cy.request({
-        //         method: 'POST',
-        //         url: '/v1/users/login', // baseUrl is prepended to url
-        //         // form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
-        //         body: {
-        //             password: 'test',
-        //             email: 'test@1.com'
-        //         }
-        //     })
-        // .then((resp) => {
-        //     // redirect status code is 302
-        //     expect(resp.status).to.eq(200)
+    //     cy.contains('button', 'Login').click()
 
+    //     cy.typeLogin({ email: 'tests1.com', password: 'tests' })
 
-        // })
+    //     cy.PostLogin()
+
+    //     cy.get('@postUser').should((resp) => {
+    //         expect(resp.status).to.eq(400)
+    //     })
+
+    // })
+    // it('Login Sucess', () => {
+
+    //     cy.contains('button', 'Login').click()
+
+    //     cy.typeLogin({ email: 'test@1.com', password: 'test' })
+
+    //     cy.PostLogin()
+
+    //     cy.get('@postUser').should((resp) => {
+    //         expect(resp.status).to.eq(200)
+    //     })
+
+    // })
+
+    it('Forget Password', () => {
+
+        cy.contains('button', 'Forgot Password').click()
 
     })
 })
+
+
+
+
+
+
+
+
+// cy.request({
+//         method: 'POST',
+//         url: '/v1/users/login', // baseUrl is prepended to url
+//         // form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+//         body: {
+//             password: 'test',
+//             email: 'test@1.com'
+//         }
+//     })
+// .then((resp) => {
+//     // redirect status code is 302
+//     expect(resp.status).to.eq(200)
+
+
+// })
