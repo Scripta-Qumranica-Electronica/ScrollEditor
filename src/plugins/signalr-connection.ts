75e03e7ffda8873632386169f0819065b986ee08
Itay Zandbank <itay@chelem.co.il>
2020-02-03 11:39:37 +0200

    Add the latest signalR backend-generated files

--- src/plugins/signalr-connection.ts
+++ src/plugins/signalr-connection.ts
@@ -1,6 +1,8 @@
 import _Vue from 'vue';
-import { HubConnectionBuilder, LogLevel, HubConnection } from '@aspnet/signalr';
+import { SignalRSQE } from '@/dtos/sqe-signalr';
+import { LogLevel } from '@microsoft/signalr';
 import { StateManager } from '@/state';
+import { EditionDTO } from '@/dtos/sqe-dtos';
 
 type ConnectionStatus = 'closed' | 'connecting' | 'connected' | 'closing';
 
@@ -8,15 +10,9 @@ export class SignalRWrapper {
     // Manage the SignalR connection. This is a singleton class
     private static _instance: SignalRWrapper;
 
-    private _connection?: HubConnection;
+    private _connection = new SignalRSQE();
     private _status = 'closed';
     private _subscribedEditionId?: number;
-
-    private constructor() {
-        if (!process.env.VUE_APP_SIGNALR_URL) {
-            throw new Error('VUE_APP_SIGNALR_URL not defined, please add it to the environment');
-        }
-    }
 
     public static get instance() {
         if (!SignalRWrapper._instance) {
@@ -24,6 +20,12 @@ export class SignalRWrapper {
         }
 
         return SignalRWrapper._instance;
+    }
+
+    private constructor() {
+        if (!process.env.VUE_APP_SIGNALR_URL) {
+            throw new Error('VUE_APP_SIGNALR_URL not defined, please add it to the environment');
+        }
     }
 
     public async subscribeEdition(editionId: number) {
@@ -40,7 +42,7 @@ export class SignalRWrapper {
             await this.unsubscribeEdition();
         }
 
-        await this._connection!.send('SubscribeToEdition', editionId);
+        await this._connection!.subscribeToEdition(editionId);
         console.debug('SubscribeToEdition called');
         this._subscribedEditionId = editionId;
     }
@@ -51,7 +53,7 @@ export class SignalRWrapper {
             return;
         }
 
-        await this._connection!.send('UnsubscribeToEdition', this._subscribedEditionId);
+        await this._connection!.unsubscribeToEdition(this._subscribedEditionId);
         console.debug('UnsubscribeToEdition called');
         this._subscribedEditionId = undefined;
     }
@@ -71,19 +73,15 @@ export class SignalRWrapper {
         if (this._connection) {
             this._connection.stop();
         }
-
-        this._connection = new HubConnectionBuilder()
-            .withUrl(process.env.VUE_APP_SIGNALR_URL!, {
-                accessTokenFactory: () => StateManager.instance.session.token || ''
-            }).configureLogging(process.env.NODE_ENV === 'development' ? LogLevel.Information : LogLevel.Error)
-            .build();
-
         this._status = 'connecting';
         try {
-            await this._connection.start();
+            await this._connection.start(process.env.VUE_APP_SIGNALR_URL!,
+                process.env.NODE_ENV === 'development' ? LogLevel.Information : LogLevel.Error,
+                StateManager.instance.session.token || '');
             this._status = 'connected';
-            this._connection!.onclose(this.onConnectionClosed);
+            this._connection.setOnConnectionClosed(this.onConnectionClosed);
             console.debug('SignalR connection opened, status is ', this._status);
+            this._connection.onUpdatedEdition(this._updatedEdition);
         } catch (error) {
             console.error("Can't connect to SignalR", error);
             this._status = 'closed';
@@ -91,11 +89,15 @@ export class SignalRWrapper {
     }
 
     private async disconnect() {
+        // Note that when removing a listener you must pass a reference to the function
+        // the listener was originally created with. You cannot use an anonymous function
+        // that happens to do the ssame thing as the (anonymous) function passed in.
+        this._connection.offUpdatedEdition(this._updatedEdition);
         console.debug('SignalR disconnect called, current status ', this._status);
 
         if (this._status === 'connected') {
             this._status = 'closing';
-            await this._connection!.stop();
+            await this._connection.stop();
         }
 
         this._status = 'closed';
@@ -109,6 +111,11 @@ export class SignalRWrapper {
         }
         this._status = 'closed';
         this._subscribedEditionId = undefined;
+    }
+
+    private _updatedEdition = (returnedData: EditionDTO) => {
+        console.debug('updated edition');
+        console.debug(returnedData);
     }
 }
 
