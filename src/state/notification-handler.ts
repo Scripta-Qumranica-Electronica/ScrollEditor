aa4655df548e77fa3eb49273fa5def6d04411e21
shaindel <shaindel.barkats@hotmail.com>
2020-02-05 22:17:13 +0200

    handleUpdatedEdition

--- src/state/notification-handler.ts
+++ src/state/notification-handler.ts
@@ -1,10 +1,21 @@
 import { NotificationHandler } from '@/dtos/temp-signalr';
 import { EditionDTO } from '@/dtos/sqe-dtos';
+import { EditionInfo } from '@/models/edition';
+import { StateManager } from '.';
 
 /* This file contains the implementation of all the incoming events from SignalR */
 
 export class SignalRNotifcationHandler extends NotificationHandler {
     public handleUpdatedEdition(edition: EditionDTO): void {
         console.debug('handleUpdateEdition ', edition);
+
+        const storedEdition = StateManager.instance.editions.find(edition.id);
+        
+        if (storedEdition) {
+            const editionInfo = new EditionInfo(edition);
+            const newEdition = {...storedEdition, ...editionInfo};
+            StateManager.instance.editions.update(newEdition);
+        }
     }
+
 }
