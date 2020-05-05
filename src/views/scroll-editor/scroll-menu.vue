864d7f899f4146d5dff1d288a155fdf7da11ed8e
shaindel <shaindel.barkats@hotmail.com>
2020-05-05 22:54:05 +0300

    remove imports not useful

--- src/views/scroll-editor/scroll-menu.vue
+++ src/views/scroll-editor/scroll-menu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="scroll-side-menu"  role="tablist">
+    <div id="scroll-side-menu" role="tablist">
         <section>
             <b-card no-body class="mb-1">
                 <b-card-header header-tag="header" class="p-1" role="tab">
@@ -10,9 +10,12 @@
                         variant="info"
                     >{{$t('misc.actions')}}</b-button>
                 </b-card-header>
-                <b-collapse id="accordion-actions" 
-                 style="display:block;"
-                accordion="my-accordion-side" role="tabpanel">
+                <b-collapse
+                    id="accordion-actions"
+                    style="display:block;"
+                    accordion="my-accordion-side"
+                    role="tabpanel"
+                >
                     <b-card-body>
                         <section class="center-btn">
                             <b-button @click="openScrollModal()">{{$t('misc.add')}}</b-button>
@@ -28,15 +31,7 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import Waiting from '@/components/misc/Waiting.vue';
-import { ImagedObject } from '@/models/imaged-object';
 import { Artefact } from '@/models/artefact';
-import ImageSettingsComponent from '@/components/image-settings/ImageSettings.vue';
-import ImagedObjectService from '../../services/imaged-object';
-import { ImageStack } from '../../models/image';
-import {
-    SingleImageSetting,
-    ImageSetting
-} from '../../components/image-settings/types';
 import ScrollModal from './scroll-modal.vue';
 
 @Component({
@@ -54,7 +49,6 @@ export default class ScrollMenu extends Vue {
 </script>
 
 <style lang="scss" scoped>
-
 #scroll-side-menu {
     touch-action: pan-y;
     top: 0;
