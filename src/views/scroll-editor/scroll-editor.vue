8dd4c1997c23f7e15d54396f3d9a31cb9ea88478
shaindel <shaindel.barkats@hotmail.com>
2020-07-18 22:56:38 +0300

    verification when write a wrong ID in the URL

--- src/views/scroll-editor/scroll-editor.vue
+++ src/views/scroll-editor/scroll-editor.vue
@@ -281,9 +281,12 @@ export default class ScrollEditor extends Vue
         // an asynchornous created to finish before calling mounted. Instead of adding a synchronization
         // between created and mounted, we just moved it to mounted.
         this.editionId = parseInt(this.$route.params.editionId, 10);
+        const edition = this.$state.editions.find(this.editionId); // Set the current scroll
+        if (!edition) {
+            this.$router.push({path: '/'});
+        }
+        this.$state.editions.current = edition;
         await this.$state.prepare.edition(this.editionId);
-        this.$state.editions.current = this.$state.editions.find(this.editionId); // Set the current scroll
-        // Shaindel - what happens if the scroll doesn't exist (write a wrong ID in the URL and see)
         this.ready = true;
 
         await this.$nextTick();
