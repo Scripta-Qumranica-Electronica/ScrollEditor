4b3a9dce8acdc9bc6bd6fe3e3d0701b0a093a6b0
shaindel <shaindel.barkats@hotmail.com>
2020-08-14 00:49:43 +0300

    Re-write the test artefacts

--- tests/e2e/specs/artefacts_spec.js
+++ tests/e2e/specs/artefacts_spec.js
@@ -8,7 +8,8 @@ describe('Imaged Artefact', function() {
 
 
     });
-
+    let afterChoice;
+    let beforeChoice;
 
     Cypress.Commands.add('typeLogin', (user) => {
         cy.get('input[type=email]')
@@ -25,25 +26,6 @@ describe('Imaged Artefact', function() {
 
     })
 
-    Cypress.Commands.add('PostRois', () => {
-        cy.server()
-        cy.route('POST', '/v1/editions/1646/rois/batch-edit').as('postPath')
-        cy.ActionButton()
-        cy.wait('@postPath')
-
-    })
-
-    // The way to get a save button
-    Cypress.Commands.add('ActionButton', () => {
-
-        cy.get('#artefact-and-buttons>.btn-menu-Artefact>.sidebarCollapse>i.fa-align-justify').click()
-        cy.get('#artefact-side-menu section:nth-child(3) div header a').click()
-        cy.get('#accordion-actions .card-body section:nth-child(2) button').click()
-
-
-    })
-
-    // After Login choose card '1QS990' 
     Cypress.Commands.add('actionAfterLogin', () => {
         cy.get('ul>li.list-item>.card').contains('1Q7Copy').first()
             .click({ multiple: true })
@@ -55,71 +37,82 @@ describe('Imaged Artefact', function() {
 
     it('Artefact textFragment ', () => {
         cy.actionAfterLogin();
+        cy.wait(2500)
         cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-align-justify').click()
-        cy.get('#my-list-id option').should('have.length', 11) /*Checks if 11 exists*/
-            .first().should('have.text', 'col. 1')
-            .next().should('have.text', 'col. 2')
-            .next().should('have.text', 'col. 3')
-            .next().should('have.text', 'col. 4')
-            .next().should('have.text', 'col. 5')
-            .next().should('have.text', 'col. 6')
-            .next().should('have.text', 'col. 7')
-            .next().should('have.text', 'col. 8')
-            .next().should('have.text', 'col. 9')
-            .next().should('have.text', 'col. 10')
-            .next().should('have.text', 'col. 11')
-        cy.get('input.select-text').type('col. 5').blur()
-        cy.get(':nth-child(5) > :nth-child(76)').click({ force: true })
-        cy.get('#my-list-id option').should('have.length', 10) /*After choose checks  if 10 exists*/
-        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-pencil-square-o').click() /*Create the shape*/
+        cy.get('#my-list-id option').should('have.length.greaterThan', 1)
+        cy.get('#my-list-id option').then((g) => {
+            beforeChoice = g.length;
+            cy.log(beforeChoice)
+        })
+        cy.server()
+        cy.route('GET', '/v1/editions/**/text-fragments/**').as('textFragmentReq')
+        cy.get('input.select-text').type('col. 1').blur()
 
-        cy.get('g#transform-root')
-            .trigger('pointermove', 290, 250)
-            .trigger('pointerdown', 290, 250)
-            .trigger('pointermove', 370, 270)
-            .trigger('pointermove', 300, 230)
-            .trigger('pointermove', 290, 250)
-            .trigger('pointerup', 290, 250)
+        cy.wait('@textFragmentReq')
+        cy.get('#text-box:first > :nth-child(1) > :nth-child(32)').click({ force: true })
+        cy.get('#my-list-id option').then((g) => {
+            afterChoice = g.length;
+            cy.log(afterChoice)
+            expect(beforeChoice).to.be.greaterThan(afterChoice);
+        })
 
-        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-trash').click() /*Delete the shape*/
+        // cy.get('[data-original-title="Menu TextFragment"] > .fa').click()
+        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-square-o').click() /*Create the shape*/
+
+        cy.get('svg.overlay')
+            .trigger('pointermove', { clientX: 150, clientY: 400 }, { force: true })
+            .trigger('pointerdown', { clientX: 150, clientY: 400 }, { force: true })
+            .trigger('pointermove', { clientX: 150, clientY: 450 }, { force: true })
+            .trigger('pointermove', { clientX: 200, clientY: 450 }, { force: true })
+            .trigger('pointerup', { clientX: 200, clientY: 450 }, { force: true })
+
+        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-trash').click({ force: true }) /*Delete the shape*/
         cy.get('input.select-text').clear()
-        cy.get('#my-list-id option').should('have.length', 11) /*And check again afeter erase if 11 exists*/
+        cy.get('#my-list-id option').then((g) => {
+            afterClear = g.length;
+            expect(beforeChoice).eq(afterClear);
+        })
         cy.get('input.select-text').type('col. 2').blur()
 
-        cy.get(':nth-child(4) > :nth-child(28)').click({ force: true })
-        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-square-o').click({ force: true })
-        cy.get('g#transform-root') /*Create the shape*/
-            .trigger('pointermove', 290, 250)
-            .trigger('pointerdown', 290, 250)
-            .trigger('pointermove', 350, 280)
-            .trigger('pointerup', 350, 280)
-        cy.get(':nth-child(14) > :nth-child(44)').click({ force: true })
+        cy.wait(2500)
+        cy.get('#accordion-0 > #text-box:first > :nth-child(1) > :nth-child(15)').click({ force: true })
+        cy.get('[data-original-title="Menu TextFragment"] > .fa').click()
+        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-pencil-square-o').click({ force: true })
+        cy.get('svg.overlay') /*Create the shape*/
+            .trigger('pointermove', { clientX: 440, clientY: 270 }, { force: true })
+            .trigger('pointerdown', { clientX: 440, clientY: 270 }, { force: true })
+            .trigger('pointermove', { clientX: 460, clientY: 418 }, { force: true })
+            .trigger('pointermove', { clientX: 460, clientY: 385 }, { force: true })
+            .trigger('pointerup', { clientX: 460, clientY: 385 }, { force: true })
         cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-play').click({ force: true }) /*Use button auto for create new shapes */
-        cy.get('g#transform-root') /*first shape */
-            .trigger('pointermove', 200, 220)
-            .trigger('pointerdown', 200, 220)
-            .trigger('pointermove', 240, 250)
-            .trigger('pointerup', 240, 250)
-        cy.get('g#transform-root') /*seconde shape*/
-            .trigger('pointermove', 150, 250)
-            .trigger('pointerdown', 150, 250)
-            .trigger('pointermove', 190, 280)
-            .trigger('pointerup', 190, 280)
-        cy.PostRois() /*Save the shape*/
+        cy.get('svg.overlay') /*first shape */
+            .trigger('pointermove', { clientX: 150, clientY: 400 }, { force: true })
+            .trigger('pointerdown', { clientX: 150, clientY: 400 }, { force: true })
+            .trigger('pointermove', { clientX: 150, clientY: 450 }, { force: true })
+            .trigger('pointermove', { clientX: 200, clientY: 450 }, { force: true })
+            .trigger('pointerup', { clientX: 200, clientY: 450 }, { force: true })
+        cy.wait(2500)
+        cy.get('svg.overlay') /*seconde shape */
+            .trigger('pointermove', { clientX: 190, clientY: 450 }, { force: true })
+            .trigger('pointerdown', { clientX: 190, clientY: 450 }, { force: true })
+            .trigger('pointermove', { clientX: 190, clientY: 500 }, { force: true })
+            .trigger('pointermove', { clientX: 220, clientY: 500 }, { force: true })
+            .trigger('pointerup', { clientX: 220, clientY: 500 }, { force: true })
+        cy.wait(2500)
         cy.get('#transform-root>g:nth-child(2)').find('path').should('have.length', 3) /*Check exict one path*/
 
-    })
-
-
-    it('save ', () => {
-        cy.get('nav.bg-dark>a.navbar-brand').click() /*Select the path I created and erased And checks not exict*/
-        cy.actionAfterLogin();
-
-        cy.wait(2500)
-        cy.get('#transform-root>g:nth-child(2)').find('path:nth-child(1)').click()
-        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-trash').click()
-        cy.ActionButton();
-        cy.get('#transform-root>g:nth-child(2)').find('path').should('have.length', 2)
+        cy.get('#transform-root>g:nth-child(2)').find('path:first').click({ force: true })
+        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-trash').click({ force: true }) /*Delete the shape*/
+        cy.get('#transform-root>g:nth-child(2)').find('path:first').click({ force: true })
+        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-trash').click({ force: true }) /*Delete the shape*/
+        cy.get('#transform-root>g:nth-child(2)').find('path:first').click({ force: true })
+        cy.get('.buttons-div.btn-tf>button.sidebarCollapse>i.fa-trash').click({ force: true }) /*Delete the shape*/
+        cy.server()
+        cy.route('POST', '/v1/editions/1646/rois/batch-edit').as('postPath')
+        cy.wait('@postPath')
+        cy.get('@postPath').should((resp) => {
+            expect(resp.status).to.eq(200)
+        })
 
     })
 })
