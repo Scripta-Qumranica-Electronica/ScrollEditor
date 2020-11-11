11a7da3655da70a04a4df8b1f82c3483a27bbb2f
shaindelb <shaindel.barkats@hotmail.com>
2020-11-11 21:45:29 +0200

    new design for scroll editor

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,7 @@ import Edition from './views/edition/Edition.vue';
 import EditionImagedObjects from './views/edition/components/imaged-objects.vue';
 import EditionArtefacts from './views/edition/components/artefacts.vue';
 import ScrollEditor from './views/scroll-editor/scroll-editor.vue';
+import ScrollEditorNew from './views/scroll-editor/scroll-editor-new.vue';
 import Registration from './views/user/Registration.vue';
 import ChangePassword from './views/user/ChangePassword.vue';
 import UpdateUser from './views/user/UpdateUser.vue';
@@ -53,6 +54,10 @@ export default new Router({
         {
             path: '/editions/:editionId/scroll-editor',
             component: ScrollEditor
+        },
+        {
+            path: '/editions/:editionId/scroll-editor/new',
+            component: ScrollEditorNew
         },
         {
             path: '/editions/:editionId/artefacts/:artefactId',
