334a2a5d009fca10d36b883381633a9db4ada099
Itay Zandbank <itay@chelem.co.il>
2021-01-16 22:48:48 +0200

    Display the EditionInfo, not the DTO

--- src/views/search/edition-results.vue
+++ src/views/search/edition-results.vue
@@ -1,14 +1,15 @@
 <template>
-    <div v-if="editions" class="scroll-bar">
+    <div v-if="editions && ready" class="scroll-bar" id="search-editions">
         <edition-list
             :title="sectionTitle"
             class="text-edition"
-            :editions="editions"
+            :editions="actualEditions"
         ></edition-list>
     </div>
 </template>
 <script lang="ts">
 import { DetailedSearchRequestDTO, EditionDTO } from '@/dtos/sqe-dtos';
+import { EditionInfo } from '@/models/edition';
 import SearchService from '@/services/search';
 import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
 import EditionsList from '../home/components/EditionsList.vue';
@@ -23,9 +24,25 @@ import { SearchFormData, SearchResults } from './types';
 export default class EditionResultsComponent extends Vue {
     @Prop( { default: null })
     private editions!: EditionDTO[] | null;
+    private ready = false;
+
+    private async mounted() {
+        this.ready = false;
+        await this.$state.prepare.allEditions();
+        this.ready = true;
+    }
+
+    private get actualEditions(): EditionInfo[] {
+        // From EditionDTO to EditionInfo - we have the editions in our store
+        if (!this.editions) {
+            return [];
+        }
+
+        return this.editions.map(ed => this.$state.editions.find(ed.id)).filter(ed => !!ed) as EditionInfo[];
+    }
 
     private get sectionTitle() {
-        return `Editions (${this.editions?.length || 0})`;
+        return `Editions (${this.actualEditions.length || 0})`;
     }
 }
 </script>
@@ -46,7 +63,7 @@ export default class EditionResultsComponent extends Vue {
     font-size: $font-size-3;
     font-family: $font-family;
 }
-.edition-list {
+.search-editions {
     padding-top: 70px;
 }
 .scroll-bar {
