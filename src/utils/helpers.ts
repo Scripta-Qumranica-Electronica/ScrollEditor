f7aef2c7c7943c5a5ba685a8400613c90e150966
Tsvia Helen Rubin <tsviar@gmail.com>
2021-02-17 23:36:48 +0200

    use this minX = Math.min(minX, x); maxX = Math.max(maxX, x);

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -28,19 +28,32 @@ export class BoundingBox implements BoundingBoxInterface {
             throw new Error("Can't combine no bounding boxes");
         }
 
-        const x1s = boxes.map(b => b.x);
-        const x1 = Math.min(...x1s);
+        // const x1s = boxes.map(b => b.x);
+        // const x1 = Math.min(...x1s);
 
-        const x2s = boxes.map(b => b.x + b.width);
-        const x2 = Math.max(...x2s);
+        // const x2s = boxes.map(b => b.x + b.width);
+        // const x2 = Math.max(...x2s);
 
-        const y1s = boxes.map(b => b.y);
-        const y1 = Math.min(...y1s);
+        // const y1s = boxes.map(b => b.y);
+        // const y1 = Math.min(...y1s);
 
-        const y2s = boxes.map(b => b.y + b.height);
-        const y2 = Math.max(...y2s);
+        // const y2s = boxes.map(b => b.y + b.height);
+        // const y2 = Math.max(...y2s);
 
-        return new BoundingBox(x1, y1, x2 - x1, y2 - y1);
+        let minX = Number.POSITIVE_INFINITY;
+        let maxX = Number.NEGATIVE_INFINITY;
+        let minY = Number.POSITIVE_INFINITY;
+        let maxY = Number.NEGATIVE_INFINITY;
+
+        for ( const box of boxes) {
+           minX = Math.min(minX, box.x);
+           minY = Math.min(minY, box.y);
+           maxX = Math.max(maxX, box.x + box.width);
+           maxY = Math.max(maxY, box.y + box.height);
+        }
+
+        // return new BoundingBox(x1, y1, x2 - x1, y2 - y1);
+        return new BoundingBox(minX, minY, maxX - minX, maxY - minY);
     }
 
     constructor(x: number = 0, y: number = 0, width: number = 0, height: number = 0) {
