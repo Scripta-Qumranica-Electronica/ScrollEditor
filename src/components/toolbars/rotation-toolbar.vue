28ff70747491c5cfc7ec48c65fa10980c2fb47aa
Itay Zandbank <itay@chelem.co.il>
2021-03-07 13:17:14 +0200

    Add instructions for the new toolbar components

---
+++ src/components/toolbars/rotation-toolbar.vue
@@ -0,0 +1,19 @@
+<!--
+
+  Create a rotation toolbar component that looks like the rotation in the artefact editor
+
+  Bind the zoom value with v-model https://www.digitalocean.com/community/tutorials/how-to-add-v-model-support-to-custom-vue-js-components
+
+  Add another property to the component called delta, with a default of 1. This is the increment or decrement when clicking on the left or right buttons
+
+  Add another property enable-text, when true the user can type the rotation angle in the textbox between the buttons, when it's false,
+  the text box is readonly
+
+  <rotation-toolbar v-model="params.rotate" delta="90" :enable-text"false"/>
+
+  Make sure the rotation angle is always between 0 and 359:
+
+  angle = ((angle % 360) + 360) % 360;
+
+  Use this component in the ARtefact Editor, Imaged Object Editor
+-->
---
+++ src/components/toolbars/zoom-toolbar.vue
@@ -0,0 +1,15 @@
+<!--
+
+  Create a zoom toolbar component that looks like the zoom in the artefact editor
+
+  Bind the zoom value with v-model https://www.digitalocean.com/community/tutorials/how-to-add-v-model-support-to-custom-vue-js-components
+
+  Add another property to the component called delta, with a default of 0.05. This is the increment or decrement when clicking on the + or - buttons
+
+  Keep in mind - the textbox inside the zoom component shows the zoom value * 100, the zoom property (in v-model) is between 0 and 1.
+
+  <zoom-toolbar v-model="params.zoom" delta="0.05"/>
+
+
+  Use this component in the ARtefact Editor, Imaged Object Editor, Scroll EditorW
+-->
