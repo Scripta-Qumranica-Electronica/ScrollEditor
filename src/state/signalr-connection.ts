62c0cb26b8cc256afc5ab95ca28e08dea9d7171e
Itay Zandbank <itay@chelem.co.il>
2021-05-31 12:10:45 +0300

    comment out long polling

--- src/state/signalr-connection.ts
+++ src/state/signalr-connection.ts
@@ -116,6 +116,7 @@ export class SignalRWrapper {
         this._connection = new HubConnectionBuilder()
             .withUrl(process.env.VUE_APP_SIGNALR_URL!, {
                 accessTokenFactory: () => StateManager.instance.session.token || '',
+                // transport: 4 // signalR.HttpTransportType.LongPolling,
             }).configureLogging(process.env.NODE_ENV === 'development' ? LogLevel.Trace : LogLevel.Error)
             .build();
         this._utils = new SignalRUtilities(this._connection);
