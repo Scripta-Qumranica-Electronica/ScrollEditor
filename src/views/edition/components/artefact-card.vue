f25e055f3e6c72fde7e76d567d029bb888940196
DeborahNacache <deborah.nacache@gmail.com>
2023-09-06 13:10:21 +0300

    rename artefact

--- src/views/edition/components/artefact-card.vue
+++ src/views/edition/components/artefact-card.vue
@@ -1,25 +1,35 @@
 <template>
     <div id="card" ref="card">
-        <router-link
-            :to="{ path: `/editions/${editionId}/artefacts/${artefact.id}` }"
-        >
-            <artefact-image
-                class="card-img-top"
-                v-if="artefact && observed"
-                :artefact="artefact"
-                maxWidth="150"
-            ></artefact-image>
-            <img class="place-holder" v-if="artefact && !observed" src="@/assets/images/rings.svg"/>
-            <label class="side-edition"
-                >{{ artefact.name }} - {{ artefact.side }}</label
-            >
+        <router-link :to="{ path: `/editions/${editionId}/artefacts/${artefact.id}` }">
+            <span class="line-name" :id="'popover-line-' + artefact.id"
+                @contextmenu="openLineMenu($event, 'popover-line-' + artefact.id)">
+                <artefact-image class="card-img-top" v-if="artefact && observed" :artefact="artefact"
+                    maxWidth="150"></artefact-image></span>
+            <img class="place-holder" v-if="artefact && !observed" src="@/assets/images/rings.svg" />
+            <label class="side-edition">{{ artefact.name }} - {{ artefact.side }}</label>
+            <b-popover custom-class="popover-sign-body" :target="'popover-line-' + artefact.id">
+                <div class="character-popover"  ref="lineMenu">
+                 <b>
+                    Rename this artefact
+                 </b>   
+                    <input ref="newArtefactName" id="newName" v-model="newArtefactName" type="text" required
+                        :placeholder="$t('home.newArtefactName')" />
+                    <div>
+                        <b-button @click="renameArtefact()" size="sm">
+                            Rename 
+                        </b-button>
+                        <b-button @click="closeLineMenu()" size="sm">Close</b-button>
+                    </div>
+
+                </div>
+            </b-popover>
         </router-link>
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+import ArtefactService from '@/services/artefact';
 import { Artefact } from '@/models/artefact';
 import ArtefactImage from '@/components/artefact/artefact-image.vue';
 
@@ -31,8 +41,11 @@ import ArtefactImage from '@/components/artefact/artefact-image.vue';
 })
 export default class ArtefactCard extends Vue {
     @Prop() public readonly artefact!: Artefact;
+    public newArtefactName: string = '';
     public observed = false;
+    private prevLineMenuId: string = '';
     private intersectionObserver?: IntersectionObserver;
+    private artefactService = new ArtefactService();
 
     public mounted() {
         this.intersectionObserver = new IntersectionObserver((entries) => this.onObserved(entries), {
@@ -41,6 +54,26 @@ export default class ArtefactCard extends Vue {
             threshold: 0.1,
         });
         this.intersectionObserver.observe(this.$refs.card as Element);
+        this.newArtefactName = this.artefact.name;
+    }
+
+    public async renameArtefact() {
+        this.artefact.name = this.newArtefactName;
+        await this.artefactService.changeArtefact(
+            this.editionId,
+            this.artefact
+        );
+        this.$root.$emit('bv::hide::popover', this.prevLineMenuId);
+
+    }
+    public closeLineMenu() {
+        this.$root.$emit('bv::hide::popover', this.prevLineMenuId);
+    }
+
+    public openLineMenu(event: MouseEvent, artefactId: any) {
+        event.preventDefault();
+        this.$root.$emit('bv::show::popover', artefactId);
+        this.prevLineMenuId = artefactId;
     }
 
     private onObserved(entries: IntersectionObserverEntry[]) {
@@ -97,6 +130,36 @@ div.card {
         font-size: $font-size-2;
         font-family: $font-family;
         color: $black;
+    }
+}
+
+.character-popover {
+    .sign-type-space:after {
+        content: '˽';
+    }
+
+    ul {
+        cursor: pointer;
+        list-style-type: none;
+        padding-left: 0px;
+
+        &:focus,
+        &:focus-visible {
+            outline: unset;
+        }
+
+        li p {
+            margin-bottom: 8px;
+        }
+
+        li p:hover {
+            color: $blue;
+        }
+    }
+
+    &:focus,
+    &:focus-visible {
+        outline: unset;
     }
 }
 
